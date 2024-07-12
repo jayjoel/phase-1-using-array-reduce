35c5ffa15f2c041b32433fd3fd4aed2189e4c4de
joel_patrick <jayjoelpn@gmail.com>
2024-07-13 00:30:25 +0300

    array reduce

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Using reduce to sum up all battery batches
+const totalBatteries = batteryBatches.reduce(function(accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+
+// Logging the result to verify
+console.log(totalBatteries); // Should log 31
+
+
